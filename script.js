0486f884efdab2441062380a87aa4ee7a6e1c85a
Sophie Senge <sophieperks@gmail.com>
2023-01-11 21:56:04 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let questions = [{question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3},
+
+{question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2}
+
+
+]
+
+let currentQuestion = 0;
